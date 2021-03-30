58df4f64a32ca3481492e7ee9fed8234f6c5bf4c
Sarrabouraui <sarra.bouraoui1@esprit.tn>
2021-03-30 10:09:29 +0100

    '30-03'

---
+++ src/app/add-hotel/add-hotel.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-hotel',
+  templateUrl: './add-hotel.component.html',
+  styleUrls: ['./add-hotel.component.css']
+})
+export class AddHotelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  showmyform(test){
+    console.log(test.value);
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { AddHotelComponent } from './add-hotel/add-hotel.component';
+import { AuthComponent } from './auth/auth.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    AddHotelComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
